1ab8ffc1e1094d0ef9a1715e03850b7ef0cb2c47
Francesco <francesco.barbato24@gmail.com>
2024-11-18 15:30:05 +0100

    a

--- src/pages/components/HeaderTop.jsx
+++ src/pages/components/HeaderTop.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 const HeaderTop = () => {
   return (
-    <div className="bg-black text-white py-2 px-4 flex justify-center text-sm">
-      <span>
+    <div className="bg-black text-white py-2 px-4 flex justify-center items-center text-sm md:text-base">
+      <span className="text-center">
         PER PRENOTARE NEL NOSTRO BARBERSHOP |{" "}
-        <a href="/Appointment" className="text-yellow-500 underline hover:text-yellow-300">
+        <a
+          href="/Appointment"
+          className="text-yellow-500 underline hover:text-yellow-300"
+        >
           CLICCARE QUI
         </a>
       </span>
