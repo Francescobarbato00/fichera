d8f691a80d16822901d9914b99c245cb559ddfc2
Francesco <francesco.barbato24@gmail.com>
2024-11-18 15:48:18 +0100

    a

--- src/pages/components/ServicesSection.jsx
+++ src/pages/components/ServicesSection.jsx
@@ -2,17 +2,16 @@ import React from "react";
 
 const ServicesSection = () => {
   return (
-    <div className="py-16">
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-0">
+    <div className="py-16 bg-white">
+      <div className="container mx-auto grid grid-cols-1 gap-8 md:grid-cols-3 md:gap-0">
         {/* Prima immagine */}
         <div className="text-center border border-white">
           <img
             src="/1.jpg" // Cambia con l'immagine appropriata
             alt="Taglio"
-            className="w-full h-80 object-cover"
+            className="w-full h-60 sm:h-80 object-cover"
           />
           <h3 className="mt-4 text-lg font-normal text-gray-800">TAGLIO</h3>
-        
         </div>
 
         {/* Seconda immagine */}
@@ -20,10 +19,9 @@ const ServicesSection = () => {
           <img
             src="/2.jpg" // Cambia con l'immagine appropriata
             alt="Barba"
-            className="w-full h-80 object-cover"
+            className="w-full h-60 sm:h-80 object-cover"
           />
           <h3 className="mt-4 text-lg font-normal text-gray-800">BARBA</h3>
-          
         </div>
 
         {/* Terza immagine */}
@@ -31,10 +29,9 @@ const ServicesSection = () => {
           <img
             src="/3.jpg" // Cambia con l'immagine appropriata
             alt="Trattamenti"
-            className="w-full h-80 object-cover"
+            className="w-full h-60 sm:h-80 object-cover"
           />
           <h3 className="mt-4 text-lg font-normal text-gray-800">TRATTAMENTI</h3>
-          
         </div>
       </div>
     </div>
