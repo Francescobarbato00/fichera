26a01d3a9e508e50ec8f9d93371fd052921ea2fa
Francesco <francesco.barbato24@gmail.com>
2024-11-21 12:57:38 +0100

    a

--- src/pages/components/ContactIntroSection.jsx
+++ src/pages/components/ContactIntroSection.jsx
@@ -2,27 +2,32 @@ import React from "react";
 
 const ContactIntroSection = () => {
   return (
-    <div
-      className="relative h-screen flex items-center justify-center text-center text-white"
-      style={{
-        backgroundImage: "url('/contatti.jpeg')", // Sostituisci con il percorso della tua immagine di sfondo
-        backgroundSize: "cover", // Assicura che l'immagine riempia interamente il contenitore
-        backgroundPosition: "center center", // Centra l'immagine sia orizzontalmente che verticalmente
-        backgroundRepeat: "no-repeat", // Evita ripetizioni
-      }}
-    >
+    <div className="relative h-[70vh] lg:h-screen flex items-center justify-center">
+      {/* Immagine di sfondo */}
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{
+          backgroundImage: "url('/contatti.jpeg')", // Sostituisci con il percorso della tua immagine
+        }}
+      ></div>
+
       {/* Overlay scuro per migliorare il contrasto */}
       <div className="absolute inset-0 bg-black opacity-70"></div>
-      <div className="relative z-10 px-4 sm:px-6">
-        <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold mb-4 sm:mb-6">
-          Contattaci
-        </h1>
-        <p className="text-base sm:text-lg md:text-xl max-w-lg sm:max-w-3xl mx-auto">
-          Siamo qui per aiutarti! Inviaci le tue domande o feedback e ti
-          ricontatteremo il prima possibile. Nel nostro barbershop ci impegniamo
-          a offrire un servizio rapido e attento per garantire un'esperienza
-          impeccabile.
-        </p>
+
+      {/* Contenuto */}
+      <div className="relative z-10 flex flex-col lg:flex-row items-center lg:items-start justify-between max-w-7xl w-full px-6 sm:px-8">
+        {/* Testo */}
+        <div className="w-full lg:w-1/2 lg:pl-16 mt-6 sm:mt-10 lg:mt-0 text-center lg:text-left">
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-white leading-tight mb-4 sm:mb-6">
+            Contattaci
+          </h1>
+          <p className="text-sm sm:text-lg text-gray-300 max-w-lg sm:max-w-xl mx-auto lg:mx-0">
+            Siamo qui per aiutarti! Inviaci le tue domande o feedback e ti
+            ricontatteremo il prima possibile. Nel nostro barbershop ci
+            impegniamo a offrire un servizio rapido e attento per garantire un'esperienza
+            impeccabile.
+          </p>
+        </div>
       </div>
     </div>
   );
